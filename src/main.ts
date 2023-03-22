14b82f8879820a99a3d47094e3d99dfef02ba14e
Vinod Kubsad <vinod.kubsad57@gmail.com>
2023-03-22 21:13:28 +0530

    Fixed cors issue locally

--- src/main.ts
+++ src/main.ts
@@ -3,9 +3,13 @@ import 'dotenv/config';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
-const port = process.env.PORT;
+const { ENV, PORT } = process.env;
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(port);
+  if (ENV === 'DEVELOPMENT') {
+    app.enableCors(); //This will be enabled only for testing
+  }
+  await app.listen(PORT);
 }
 bootstrap();
